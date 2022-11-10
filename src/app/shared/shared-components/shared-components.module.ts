6d8f71274b773b223dcaa01e09d8fecc4d342a6d
sabaapxazava <60016171+sabaapxazava@users.noreply.github.com>
2022-11-10 22:09:41 +0400

    Header And Footer

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const shared:any = [
   CommonModule,
-  CardComponent
+  CardComponent,
+  HeaderComponent,
+  FooterComponent
 ]
 
 @NgModule({
   declarations:[
-    CardComponent
+    CardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
   ],
