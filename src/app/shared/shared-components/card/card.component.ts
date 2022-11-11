3521c466fe2644cb2bfa13e772a3e7da95ab2af0
Onise Soselia <soseliaonise@gmail.com>
2022-11-12 00:52:50 +0400

    Add a slider to the card

--- src/app/shared/shared-components/card/card.component.ts
+++ src/app/shared/shared-components/card/card.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  card = {
+    img: [
+      'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+      'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    ],
+    title: 'Hotel name',
+    reiting: '4.5',
+    price: 380,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
