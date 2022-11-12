a81d3d60e478ff6e14adaa59b362805ec167a9e6
Onise Soselia <soseliaonise@gmail.com>
2022-11-12 18:11:25 +0400

    I finished card

--- src/app/shared/shared-components/card/card.component.ts
+++ src/app/shared/shared-components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,18 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  cards = [
-    {
-      img: [
-        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
-        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      ],
-      title: 'Los Veneros, Punta Mita, Mexico',
-      reiting: '4.5',
-      price: 380,
-    },
-  ];
+  @Input() card: any;
+
+  // cards = [
+  //   {
+  //     img: [
+  //       'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+  //       'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  //       'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  //       'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+  //       'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  //     ],
+  //     title: 'Los Veneros, Punta Mita, Mexico',
+  //     reiting: '4.5',
+  //     price: 380,
+  //   },
+  // ];
 
   selectedIndex = 0;
 
@@ -30,23 +34,23 @@ export class CardComponent implements OnInit {
     this.selectedIndex = index;
   }
 
-  onPrevClick(){
-    if(this.selectedIndex == 0){
-      this.selectedIndex = this.cards[0].img.length - 1
-    }else{
-      this.selectedIndex--
+  onPrevClick() {
+    if (this.selectedIndex == 0) {
+      this.selectedIndex = this.card.img.length - 1;
+    } else {
+      this.selectedIndex--;
     }
   }
 
-  onNextClick(){
-    if(this.selectedIndex === this.cards[0].img.length - 1){
-      this.selectedIndex = 0 
-    }else{
-      this.selectedIndex++
+  onNextClick() {
+    if (this.selectedIndex === this.card.img.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
     }
   }
 
-  ono(){
-    console.log('add wishlist')
+  addCardInWishlist() {
+    console.log('add wishlist');
   }
 }
