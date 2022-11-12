fa4534bf2caa3bc8dbd27661a2c13f805f50792b
Onise Soselia <soseliaonise@gmail.com>
2022-11-12 15:11:46 +0400

    I added a test card

--- src/app/shared/shared-components/card/card.component.ts
+++ src/app/shared/shared-components/card/card.component.ts
@@ -6,18 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  card = {
-    img: [
-      'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
-      'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    ],
-    title: 'Hotel name',
-    reiting: '4.5',
-    price: 380,
-  };
+  cards = [
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+  ];
+
+  selectedIndex = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  // dot click
+  selectImage(index: number) {
+    this.selectedIndex = index;
+  }
+
+  onPrevClick(){
+    if(this.selectedIndex == 0){
+      this.selectedIndex = this.cards[0].img.length - 1
+    }else{
+      this.selectedIndex--
+    }
+  }
+
+  onNextClick(){
+    if(this.selectedIndex === this.cards[0].img.length - 1){
+      this.selectedIndex = 0 
+    }else{
+      this.selectedIndex++
+    }
+  }
 }
