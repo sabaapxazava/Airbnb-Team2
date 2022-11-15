3da5c265ea3cc19357248c51599c6f6a1be81d11
Onise Soselia <soseliaonise@gmail.com>
2022-11-15 20:20:24 +0400

    I finish cards component on the home page

--- src/app/views/home-page/most-requested-regions/most-requested-regions.component.ts
+++ src/app/views/home-page/most-requested-regions/most-requested-regions.component.ts
@@ -1,15 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-most-requested-regions',
   templateUrl: './most-requested-regions.component.html',
-  styleUrls: ['./most-requested-regions.component.scss']
+  styleUrls: ['./most-requested-regions.component.scss'],
 })
 export class MostRequestedRegionsComponent implements OnInit {
+  cards = [
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+    {
+      img: [
+        'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+        'https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/2017802/pexels-photo-2017802.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      ],
+      title: 'Los Veneros, Punta Mita, Mexico',
+      reiting: '4.5',
+      price: 380,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
