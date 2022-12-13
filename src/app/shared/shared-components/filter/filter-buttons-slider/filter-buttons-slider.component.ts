a550f36cd8fa8dace567924a98172fb46dfc8164
Kukhianidze <KUKH004@dx1.admgt.net>
2022-12-13 11:51:29 +0400

    add filter function by click on icons

--- src/app/shared/shared-components/filter/filter-buttons-slider/filter-buttons-slider.component.ts
+++ src/app/shared/shared-components/filter/filter-buttons-slider/filter-buttons-slider.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/shared/shared-services/category.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-filter-buttons-slider',
@@ -14,7 +15,10 @@ export class FilterButtonsSliderComponent implements OnInit {
   prevScrollLeft!: any;
   positionDiff!: any;
 
-  constructor(private categoryService: CategoryService) {}
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.sliderIcons = document.querySelector('.slider-icons-wrapper');
@@ -47,5 +51,9 @@ export class FilterButtonsSliderComponent implements OnInit {
   }
   onScrollRight() {
     this.sliderIcons.scrollLeft += 500;
+  }
+  onCategoriesClick(item: any) {
+    this.categoryService.categoryItemEmitter.emit();
+    this.router.navigate(['filtered-hotels', item.name]);
   }
 }
--- src/app/shared/shared-services/category.service.ts
+++ src/app/shared/shared-services/category.service.ts
@@ -2,11 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
+  categoryItemEmitter: EventEmitter<void> = new EventEmitter();
+
   private apiUrl!: string;
 
   constructor(private httpClient: HttpClient) {
