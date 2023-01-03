93a6375d55a8d2fdb0db6139cc7a64774df0c8bc
Onise Soselia <soseliaonise@gmail.com>
2023-01-03 22:47:36 +0400

    add user form in profileComponent

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  userInfo = new FormGroup({
+    email: new FormControl('soseliaonise@gmail.com'),
+    fullName: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    gender: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.userInfo.value);
   }
-
 }
